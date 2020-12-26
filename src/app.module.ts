f38c16d3a8f43dedbfafb98ff3164045e8df925b
dngwoo <akfmzk84@naver.com>
2020-12-27 01:24:54 +0900

    Modules and Dependency Injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app/app.controller';
 
 // 이 함수는 데코레이터라고 부른다.
 // 데코레이터는 클래스에 함수기능을 추가할 수 있다.
 @Module({
-  imports: [],
+  imports: [MoviesModule], // main.ts에서 하나의 모듈로 합쳐짐
   // 컨트롤러가 하는 일은 기본적으로 url을 가져오고 함수를 실행하는 것이다.
   // express로 치면 라우터같은 존재임.(controller + router)
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   // AppModule은 rootModule와 비슷하다. 어떤 서비스를 만들던 import해주어야 한다.
+  // app.moudle.ts에서 imports : [] 에 들어가는 모든 모듈들을 여기서 하나의 모듈로 합쳐준다.
   const app = await NestFactory.create(AppModule);
   // pipe라는 것은 express에서 middleware 같은 것이다.
   app.useGlobalPipes(
