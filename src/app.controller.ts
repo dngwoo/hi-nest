55ba0362625490d42ac54e9ca35978f1898497cd
dngwoo <akfmzk84@naver.com>
2020-12-26 10:18:00 +0900

    Controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,16 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get이라는 것은 express의 get라우터와 같은 역할을 한다.(app.get)
+  // 데코레이터는 꾸며주는 함수나 클래스랑 붙어 있어야 한다.
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // 예제
+  @Get('/hello')
+  sayHello(): string {
+    return 'Hello everyone';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { AppService } from './app.service';
 // 데코레이터는 클래스에 함수기능을 추가할 수 있다.
 @Module({
   imports: [],
+  // 컨트롤러가 하는 일은 기본적으로 url을 가져오고 함수를 실행하는 것이다.
+  // express로 치면 라우터같은 존재임.(controller + router)
   controllers: [AppController],
   providers: [AppService],
 })
