69d2959741743a0e818bfecea3d3772b85da9a54
dngwoo <akfmzk84@naver.com>
2020-12-26 10:06:23 +0900

    Overview

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// 이 함수는 데코레이터라고 부른다.
+// 데코레이터는 클래스에 함수기능을 추가할 수 있다.
 @Module({
   imports: [],
   controllers: [AppController],
